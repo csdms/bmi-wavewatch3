f56da3170c562ade014691a951310a57291f1517
mcflugen <mcflugen@gmail.com>
2022-06-02 15:06:41 -0600

    add tests for ww3 clean command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 from click.testing import CliRunner
 
@@ -15,15 +17,30 @@ def test_command_line_interface():
     assert "version" in result.output
 
 
-@pytest.mark.parametrize("subcommand", ("url", "fetch"))
+@pytest.mark.parametrize("subcommand", ("url", "fetch", "clean"))
 def test_subcommand_help(subcommand):
     runner = CliRunner()
     result = runner.invoke(ww3, [subcommand, "--help"])
     assert result.exit_code == 0
 
 
-@pytest.mark.parametrize("subcommand", ("url", "fetch"))
+@pytest.mark.parametrize("subcommand", ("url", "fetch", "clean"))
 def test_noop(subcommand):
     runner = CliRunner()
     result = runner.invoke(ww3, [subcommand])
     assert result.exit_code == 0
+
+
+def test_clean(tmpdir):
+    runner = CliRunner()
+
+    with tmpdir.as_cwd():
+        data_file = pathlib.Path("multi_1.glo_30m.dp.201712.grb2")
+        data_file.touch()
+
+        assert data_file.is_file()
+        result = runner.invoke(ww3, ["clean", "--cache-dir=.", "--dry-run"])
+        assert data_file.is_file()
+
+        result = runner.invoke(ww3, ["clean", "--cache-dir=."])
+        assert not data_file.is_file()
