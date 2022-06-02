3bc93645c597ae1dbbf468cf658ebc4ea134c5f0
mcflugen <mcflugen@gmail.com>
2022-06-02 10:41:58 -0600

    add WaveWatch3 class

--- wavewatch3/__init__.py
+++ wavewatch3/__init__.py
@@ -1,13 +1,16 @@
 from ._version import __version__
 from .bmi import BmiWaveWatch3
 from .downloader import WaveWatch3Downloader
-from .url import WaveWatch3URL
 from .errors import ChoiceError, WaveWatch3Error
+from .url import WaveWatch3URL
+from .wavewatch3 import WaveWatch3
+
 
 __all__ = [
     "__version__",
     "BmiWaveWatch3",
     "ChoiceError",
+    "WaveWatch3",
     "WaveWatch3Downloader",
     "WaveWatch3Error",
     "WaveWatch3URL",
