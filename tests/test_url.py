62fdb080e5ac42b2ef4bece183b0b5d1412dd9f6
mcflugen <mcflugen@gmail.com>
2022-06-06 09:19:53 -0600

    add more tests with all sources

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,63 +1,73 @@
+import datetime
+import itertools
+
 import pytest
 
 import wavewatch3 as ww3
 
 
-@pytest.mark.parametrize("source", ["multigrid"])
-@pytest.mark.parametrize("grid", ww3.SOURCES["multigrid"].GRIDS)
-@pytest.mark.parametrize("quantity", ww3.SOURCES["multigrid"].QUANTITIES)
+@pytest.mark.parametrize(
+    "source,grid,quantity",
+    [
+        (_source,) + _grid_and_quantity
+        for _source in ww3.SOURCES.values()
+        for _grid_and_quantity in itertools.product(_source.GRIDS, _source.QUANTITIES)
+    ],
+)
 def test_url(source, grid, quantity):
-    url = ww3.SOURCES[source]("2010-05-22", quantity, grid=grid)
-    assert url.year == 2010
-    assert url.month == 5
+    date = datetime.date.fromisoformat(source.MIN_DATE)
+    url = source(date.isoformat(), quantity, grid=grid)
+    assert url.year == date.year
+    assert url.month == date.month
     assert url.quantity == quantity
     assert url.grid == grid
-    assert url.filename == f"multi_1.{grid}.{quantity}.201005.grb2"
+    assert f"{grid}.{quantity}.{date.year}{date.month:02}.grb" in url.filename
 
 
-@pytest.mark.parametrize("source", ["multigrid"])
-@pytest.mark.parametrize("quantity", ww3.SOURCES["multigrid"].QUANTITIES)
+@pytest.mark.parametrize(
+    "source,quantity",
+    [
+        (_source, _quantity)
+        for _source in ww3.SOURCES.values()
+        for _quantity in _source.QUANTITIES
+    ],
+)
 def test_url_default_grid(source, quantity):
-    url = ww3.SOURCES[source]("2010-05-22", quantity)
-    assert url.grid == "glo_30m"
+    url = source(source.MIN_DATE, quantity)
+    assert url.grid in source.GRIDS
 
 
-@pytest.mark.parametrize("source", ww3.SOURCES)
+@pytest.mark.parametrize("source", ww3.SOURCES.values())
 def test_url_str(source):
-    Source = ww3.SOURCES[source]
-    url = Source(Source.MIN_DATE, "wind")
-    assert str(url).startswith(Source.SCHEME)
+    url = source(source.MIN_DATE, "wind")
+    assert str(url).startswith(source.SCHEME)
 
 
-@pytest.mark.parametrize("source", ww3.SOURCES)
+@pytest.mark.parametrize("source", ww3.SOURCES.values())
 def test_url_repr(source):
     import wavewatch3  # noqa
 
-    Source = ww3.SOURCES[source]
-    url = Source(Source.MIN_DATE, "tp")
+    url = source(source.MIN_DATE, "tp")
     assert eval(repr(url)) == url
 
 
-@pytest.mark.parametrize("source", ww3.SOURCES)
+@pytest.mark.parametrize("source", ww3.SOURCES.values())
 def test_url_quantity_setter(source):
-    Source = ww3.SOURCES[source]
-    url = Source(Source.MIN_DATE, "tp")
+    url = source(source.MIN_DATE, "tp")
     url.quantity = "dp"
     assert url.quantity == "dp"
-    assert url == Source(Source.MIN_DATE, "dp")
+    assert url == source(source.MIN_DATE, "dp")
 
     with pytest.raises(ww3.ChoiceError):
         url.quantity = "foo"
 
 
-@pytest.mark.parametrize("source", ww3.SOURCES)
+@pytest.mark.parametrize("source", ww3.SOURCES.values())
 def test_url_grid_setter(source):
-    Source = ww3.SOURCES[source]
-
-    url = Source(Source.MIN_DATE, "tp")
-    for grid in Source.GRIDS:
+    url = source(source.MIN_DATE, "tp")
+    for grid in source.GRIDS:
         url.grid = grid
-        assert url == Source(Source.MIN_DATE, "tp", grid=grid)
+        assert url == source(source.MIN_DATE, "tp", grid=grid)
 
     with pytest.raises(ww3.ChoiceError):
         url.grid = "bar"
